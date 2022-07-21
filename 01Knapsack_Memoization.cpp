9265c4c6d0604a5e8fd6a2f3b28ec741119a6836
Arpan <mailmepurabi78@gmail.com>
2022-07-21 23:18:18 +0530

    Bottom-up_knapsack Added

--- 01Knapsack_Memoization.cpp
+++ 01Knapsack_Memoization.cpp
@@ -21,11 +21,11 @@ using namespace std;
 
 
 
-int knapSack(int val[],int w[],int W,int n){
+int knapSack(int val[],int w[],int W,int n,vector<vector<int>> &dp){
     if(n==0||W==0) return 0;
-
-    if(w[n-1]<=W) return max(knapSack(val,w,W-w[n-1],n-1)+val[n-1],knapSack(val,w,W,n-1));
-    else if(w[n-1]>W) return knapSack(val,w,W,n-1);
+    if(dp[n][W]!=-1) return dp[W][n];
+    if(w[n-1]<=W) return dp[W][n]=max(knapSack(val,w,W-w[n-1],n-1,dp)+val[n-1],knapSack(val,w,W,n-1,dp));
+    else if(w[n-1]>W) return dp[W][n]=knapSack(val,w,W,n-1,dp);
 }
 
 int main()
@@ -36,13 +36,14 @@ int main()
     cout<<"Enter the number of items:"<<endl;
     cin>>n;
     int val[n],w[n];
+    vector<vector<int>> dp(W + 1, vector<int>(n + 1, -1));
     for (int i = 0; i < n; i++)
     {
         cout<<"Enter weight and value for the item:"<<endl;
         cin>>w[i]>>val[i];
     }
 
-    cout<<"Max Profit is "<<knapSack(val,w,W,n)<<endl;
+    cout<<"Max Profit is "<<knapSack(val,w,W,n,dp)<<endl;
     
     return 0;
 }
